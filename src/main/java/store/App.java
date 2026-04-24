ff2d61c8a1079461591084799df04bc39b8a4d21
natefrezzell <nateb9086@gmail.com>
2026-04-24 14:31:52 -0400

    up to date

--- src/main/java/store/App.java
+++ src/main/java/store/App.java
@@ -2,9 +2,10 @@ package store;
 import java.util.Scanner;
 import java.io.*;
 import java.util.regex.Pattern;
-public class app {
-    public static void main(String[] args) {
+import java.util.HashMap;
 
+public class App {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to the online store!");
@@ -14,7 +15,6 @@ public class app {
     public static void homeScreen(Scanner scanner) {
 
         while (true) {
-
             System.out.println("Select from the following option: ");
             System.out.println("\t1) Display Products");
             System.out.println("\t2) Display Cart");
@@ -25,12 +25,11 @@ public class app {
 
             switch (userOption) {
                 case 1:
+                    displayProducts(scanner);
                     formatSpaces();
                     break;
-
                 case 2:
                     formatSpaces();
-                    ;
                     break;
                 case 0:
                     System.out.println("Thanks for visiting my online store!!!");
@@ -38,18 +37,60 @@ public class app {
                     System.exit(0);
                     break;
                 default:
-                    System.out.println("Incorrect option entered");
+                    System.out.println("Incorrect option entered(press ENTER ");
                     scanner.nextLine();
                     formatSpaces();
             }
-
         }
     }
-    public static void displayProducts(Scanner scanner){
+    public static void displayProducts(Scanner scanner) {
+        HashMap<String, Product> products = loadInventory();
+
+        boolean run = true;
+        while (run) {
+            System.out.println("Select from the following options: ");
+            System.out.println("\t1) View All Products");
+            System.out.println("\t2) Search Products");
+            System.out.println("\t3) Add Product");
+            System.out.println("\t0) Go Back");
+            System.out.print("Enter your selection: ");
+            int userOption = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (userOption) {
+                case 1:
+                    for (Product p : products.values()) {
+                        System.out.printf("%s: $%.2f%n", p.getProductName(), p.getPrice());
+                    }
+                    formatSpaces();
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+                    break;
+                case 0:
+                    formatSpaces();
+                    run = false;
+                    break;
+                default:
+                    System.out.println("Incorrect option entered (press ENTER to continue) ");
+                    scanner.nextLine();
+                    formatSpaces();
+
+
+            }
+        }
+    }
+
+    public static HashMap<String, Product> loadInventory () {
+        HashMap<String, Product> products = new HashMap<> ();
+
         try{
             BufferedReader buffReader = new BufferedReader(new FileReader("src/main/resources/products.csv"));
 
             String productItem;
+            buffReader.readLine();
             while((productItem = buffReader.readLine()) != null) {
                 String[] splitProductItem = productItem.split(Pattern.quote("|" ));
 
@@ -59,12 +100,17 @@ public class app {
                 String department = splitProductItem[3];
 
                 Product product = new Product(sku, productName, department, price);
+                products.put(sku, product);
+
             }
+            buffReader.close();
         }
-        catch (IOException e ) {
+        catch (IOException e) {
             e.printStackTrace();
         }
+        return products;
     }
+
     public static void formatSpaces() {
         System.out.print("\n\n");
     }
