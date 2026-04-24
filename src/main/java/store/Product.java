dd310eb0c98cbbb837110ba8edf7624081be30b5
natefrezzell <nateb9086@gmail.com>
2026-04-24 09:30:31 -0400

    added scanner

--- src/main/java/store/Product.java
+++ src/main/java/store/Product.java
@@ -1,4 +1,29 @@
 package store;
 
 public class Product {
+    private String sku, productName, department;
+    private double price;
+
+    public Product(String sku, double price, String department, String productName) {
+        this.sku = sku;
+        this.price = price;
+        this.department = department;
+        this.productName = productName;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
--- src/main/java/store/app.java
+++ src/main/java/store/app.java
@@ -1,4 +1,16 @@
 package store;
-
+import java.util.Scanner;
 public class app {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the online store!");
+        System.out.println("Select from the following option: ");
+        System.out.println("\t1) Display Products");
+        System.out.println("\t2) Display Cart");
+        System.out.println("\t0) Exit");
+        System.out.print("Enter your selection: ");
+        int userOption = scanner.nextInt();
+
+    }
 }
